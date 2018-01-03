388e597cb780fecae74a6bf7106b68777f04f4c3
zhao <45147851@qq.com>
2018-01-03 15:41:02 +0800

    step2 finished...

--- sps_main.py
+++ sps_main.py
@@ -27,19 +27,6 @@ TILESIZE = 80
 XMARGIN = int((WINDOWWIDTH - (TILESIZE * BOARDWIDTH + (BOARDWIDTH - 1))) / 2)
 YMARGIN = int((WINDOWHEIGHT - (TILESIZE * BOARDHEIGHT + (BOARDHEIGHT - 1))) / 2)
 
-def main():
-    global FPSCLOCK, DISPLAYSURF
-
-    pygame.init()
-    FPSCLOCK = pygame.time.Clock()
-    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-    pygame.display.set_caption('Slide Puzzle')
-
-    while True:
-        checkForQuit()
-
-        pygame.display.update()
-        FPSCLOCK.tick(FPS)
 
 def checkForQuit():
     for event in pygame.event.get(QUIT):
@@ -49,9 +36,58 @@ def checkForQuit():
             terminate()
         pygame.event.post(event)
 
+
 def terminate():
     pygame.quit()
     sys.exit()
+
+
+def drawTile(tilex, tiley, number):
+    left, top = getLeftTopOfTile(tilex, tiley)
+    pygame.draw.rect(DISPLAYSURF, TILECOLOR, (left, top, TILESIZE, TILESIZE))
+    textSurf = BASICFONT.render(str(number), True, TEXTCOLOR)
+    textRect = textSurf.get_rect()
+    textRect.center = left + int(TILESIZE/2) , top + int(TILESIZE/2)
+    DISPLAYSURF.blit(textSurf, textRect)
+
+def getLeftTopOfTile(tileX, tileY):
+    left = XMARGIN + (tileX * TILESIZE) + (tileX -1)
+    top = YMARGIN + (tileY * TILESIZE) + (tileY -1)
+    return (left, top)
+
+def drawBoard(board):
+    DISPLAYSURF.fill(BGCOLOR)
+    for tilex in range(len(board)):
+        for tiley in range(len(board[0])):
+            if board[tilex][tiley]:
+                drawTile(tilex, tiley, board[tilex][tiley])
+
+    left, top = getLeftTopOfTile(0, 0)
+    width = BOARDWIDTH * TILESIZE
+    height = BOARDHEIGHT * TILESIZE
+    pygame.draw.rect(DISPLAYSURF, BOARDCOLOR, \
+                     (left-5, top-5, width+11, height+11), 4)
+
+
+def main():
+    global FPSCLOCK, DISPLAYSURF, BASICFONT
+
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    pygame.display.set_caption('Slide Puzzle')
+    BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
+
+    board = [[3,2], [1, None]]
+
+    while True:
+        checkForQuit()
+        drawBoard(board)
+
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+
 
 if __name__ == '__main__':
     main()
